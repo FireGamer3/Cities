2db9e92b519ce05be7b8f95e98cc5d9bea558b7d
FireGamer3 <gm31334@gmail.com>
2017-09-30 13:07:09 -0500

    stuff

--- Cities/Assets/Scripts/Map/Map.cs
+++ Cities/Assets/Scripts/Map/Map.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Map : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject TilePrefab;
+    public Material[] TileTerrain;
+    void Start () {
+        GenerateMap();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void GenerateMap() {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                Tile t = new Tile(x, y);
+                GameObject tileGO = Instantiate(TilePrefab, t.Position(), Quaternion.identity, this.transform);
+                MeshRenderer mr = tileGO.GetComponentInChildren<MeshRenderer>();
+                mr.material = TileTerrain[Random.Range(0, TileTerrain.Length)];
+            }
+        }
+    }
 }
---
+++ Cities/Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines a Tile
+/// </summary>
+public class Tile {
+    public readonly int x;
+    public readonly int y;
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Vector3 Position() {
+        return new Vector3(x, 0, y);
+    }
+}
